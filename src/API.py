5f5d77bc29ffb222cc849ede7f8a45f8511ed1b4
Leandro <thegames479@gmail.com>
2024-04-18 16:38:53 -0300

    0.0.6
    Se preparó la API para que ande en un docker. Procedemos a testear con el frontend.

--- src/API.py
+++ src/API.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 from typing_extensions import Annotated
 from pydantic import BaseModel
 import app.dbRequests as dbRequests
@@ -13,6 +14,14 @@ tags_metadata = [
 ]
 
 app = FastAPI(openapi_tags=tags_metadata)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 
 # CONTENEDORES
